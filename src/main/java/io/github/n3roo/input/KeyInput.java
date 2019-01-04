71a04cea6489b3cbe9d5add5cfe566f5910d7ad2
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2019-01-04 15:10:23 +0100

    keyinput: improve keyinput class to support any key code

--- src/main/java/io/github/n3roo/input/KeyInput.java
+++ src/main/java/io/github/n3roo/input/KeyInput.java
@@ -3,19 +3,34 @@ package io.github.n3roo.input;
 import com.jogamp.newt.event.KeyEvent;
 import com.jogamp.newt.event.KeyListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class KeyInput implements KeyListener {
 
-    private static boolean[] keys = new boolean[256];
+    private static Map<Short, Boolean> keys = new HashMap<Short, Boolean>();
 
+    /**
+     * Called every time a key is pressed.
+     * @param keyEvent all the things about the event are stored in this variable.
+     */
     public void keyPressed(KeyEvent keyEvent) {
-        keys[keyEvent.getKeyCode()] = true;
+        keys.put(keyEvent.getKeyCode(), true);
     }
 
+    /**
+     * Called every time a key is released.
+     * @param keyEvent all the things about the event are stored in this variable.
+     */
     public void keyReleased(KeyEvent keyEvent) {
-        keys[keyEvent.getKeyCode()] = false;
+        keys.put(keyEvent.getKeyCode(), false);
     }
 
-    public static boolean getKey(int keyCode){
-        return keys[keyCode];
+    /**
+     * @param keyCode the code of the key. (jogamp key code, and not something else).
+     * @return true if the given key is being pressed.
+     */
+    public static boolean getKey(short keyCode){
+        return keys.get(keyCode);
     }
 }
